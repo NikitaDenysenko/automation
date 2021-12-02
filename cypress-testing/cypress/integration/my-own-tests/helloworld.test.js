f9d2e54d1c34446b45c1d8eba502c0c5feb66a0e
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-12-02 08:40:56 +0200

    added tests

--- cypress-testing/cypress/integration/my-own-tests/helloworld.test.js
+++ cypress-testing/cypress/integration/my-own-tests/helloworld.test.js
@@ -1,12 +1,29 @@
 /// <reference types="cypress" /> //provides autocomplite functionality 
 
 describe('Basic Test', () => {
-    it('We have correct page title', () => {
+    it('Web page loads', () => {
         cy.visit('https://www.freecodecamp.org/')
-        cy.contains('Learn to code').should('have.text', 'Learn to code — for free.')
-        //cy.get('nav#universal-nav')
-        cy.get('.toggle-button-nav').click()
-        cy.viewport('iphone-6')
+    })
+    it('Shoud open login page', () => {
+        cy.contains("Get started (it's free)").click()
+        cy.get('#email').should('exist')
+        cy.contains('Continue with Google').should('exist')
+        cy.contains('Continue with GitHub').should('exist')
+    })
+    it('Login page link test', () => {
+        cy.url().should('include', '/login')
+        cy.url().then(value => {
+            cy.log('Current url is: ', value)
+        })
+        cy.get('#email').type('test@test.com')
+    })
+    it('Shoud complite task from the course', () => {
+        cy.visit('https://www.freecodecamp.org/learn')
+        cy.contains('Responsive Web Design Certification').click()
+        cy.contains('Declare the Doctype of an HTML Document').click()
+        cy.get('.view-line').type('<!DOCTYPE html><html><h1></h1></html>')
+        cy.contains('Run the Tests').click()
+        cy.contains('Go to next challenge').click()
     })
 })
 
